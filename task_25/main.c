1093cf2fa33ae6a23a8d1e270bd7fbf12256a57c
goglom <57570818+goglom@users.noreply.github.com>
2020-12-16 23:22:19 +0700

    Update main.c
    rewrite all code :+1:

--- task_25/main.c
+++ task_25/main.c
@@ -4,13 +4,19 @@
 #include <errno.h>
 #include <ctype.h>
 #include <sys/wait.h>
-
+#include <stdlib.h>
 
 #define BUF_SIZE 64
 
-
-
-int read_toupper_write(int read_fd, int write_fd)
+void make_upper(char* buff, size_t n)
+{
+	for(size_t i = 0; i < n; ++i)
+		buff[i] = toupper(buff[i]);
+}
+void void_editor(char*, size_t)
+{
+}
+int redirect(int read_fd, int write_fd, void (*editor)(char*, size_t))
 {
 	errno = 0;
 	char buf[BUF_SIZE] = {0};
@@ -23,19 +29,22 @@ int read_toupper_write(int read_fd, int write_fd)
 			errno = 0;
 			continue;
 		}
-		else
+		else if (count == -1)
 		{
 			perror("Error in reading from pipe");
+			return -1;
 		}
-		write(STDOUT_FILENO, 
+		editor(buf, count);
+		
+		if (write(STDOUT_FILENO, buf, count) == -1)
+		{
+			perror("read_toupper_write() - error while writing to write_fd");
+			return -2;
+		}
 	}
-	return count;
+	return 0;
 }
-void makeUpper(char* buff, size_t n)
-{
-	for(size_t i = 0; i < n; ++i)
-		buff[i] = toupper(buff[i]);
-}
+
 int close_pipe(int pfildes[2])
 {
 	errno = 0;
@@ -50,16 +59,12 @@ int close_pipe(int pfildes[2])
 int main()
 {
 	int p_filedes[2];
-//	const char* msg = "Hello, world!\n";
-//	int len = strlen(msg) + 1;
-
+	
 	if(pipe(p_filedes) == -1)
 	{
 		perror("Error in initializing pipe");
 		return -1;
 	}
-	close(STDIN_FILENO);
-	dup2(p_filedes[0], STDIN_FILENO);
 
 	pid_t rpid = fork();
 	if(rpid == -1)
@@ -68,35 +73,36 @@ int main()
 		close_pipe(p_filedes);
 		return -1;
 	}
+	/*
+		*	Child branch
+	*/
 	else if(rpid == 0)
 	{
-		char buffer[MAXBUFF];
-		int len = readFromPipe(p_filedes[1], buffer);
-		makeUpper(buffer, len);
-
-		if(write(1, buffer, len) != len)
-		{
-			perror("Error in stdout print in read child process");
-			close_pipe(p_filedes);
-			return -1;
-		}
-		close_pipe(p_filedes);
-		return 0;
+		return redirect(p_filedes[1], STDOUT_FILENO, make_upper)
+			? EXIT_FAILURE
+			: EXIT_SUCCESS;
 	}
-	int rstat;
-
-	while(wait(&rstat) == -1)
+	/*
+		* Parent branch
+	*/
+	if (redirect(STDIN_FILENO, p_filedes[0], void_editor))
+	{
+		close_pipe(p_filedes);
+		return EXIT_FAILURE;
+	}
+	int ch_stat;
+	while(wait(&ch_stat) == -1)
 	{
 		if(errno == EINTR)
 		{
 			errno = 0;
 			continue;
 		}
-		perror("Error in read");
+		perror("Error while waitin child process");
 		close_pipe(p_filedes);
-
 		return 1;
 	}
+	
 	close_pipe(p_filedes);
 	return 0;
 }
