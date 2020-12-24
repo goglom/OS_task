bb174b4520056888b1ebd1e6373157f96ad84dce
goglom <goglom12@gmail.com>
2020-12-24 22:39:11 +0700

    update main.c

--- task_26/main.c
+++ task_26/main.c
@@ -3,7 +3,7 @@
 #include <ctype.h>
 #include <wait.h>
 
-#define BUFFER_SIZE 100
+#define BUFFER_SIZE 1024
 
 void make_upper(char* buff, size_t n)
 {
@@ -22,33 +22,33 @@ int my_pclose(FILE* pipe)
 	if (WIFEXITED(status))
 	{
 		int exit_status = WEXITSTATUS(status);
-		printf("exited, status=%d:\tchild_pid=%d\n\n", exit_status, pid);
 		if (exit_status != 0)
 		{
+			printf("Child exited wtih status: %d\n", exit_status);
 			return -1;
 		}
 	}
 	else if (WIFSIGNALED(status))
 	{
-		printf("killed by signal %d:\tchild_pid=%d\n\n", WTERMSIG(status), pid);
+		printf("killed by signal\n", WTERMSIG(status));
 		return -1;
 	}
 	else if (WIFSTOPPED(status))
 	{
-		printf("stopped by signal %d:\tchild_pid=%d\n\n", WSTOPSIG(status), pid);
+		printf("stopped by signal: %d\n", WSTOPSIG(status));
 		return -1;
 	}
 	else if (WIFCONTINUED(status))
 	{
-		printf("continued\n\n");
+		printf("continued\n");
 	}
 	return 0;
 }
-	
+
 int main()
 {
 	FILE* input = popen("cat input.txt", "r");
-	
+
 	if (input == NULL)
 	{
 		perror("Error: popen()");
@@ -56,7 +56,7 @@ int main()
 	}
    	char buf[BUFFER_SIZE];
 	size_t read_count = fread(buf, sizeof(char), BUFFER_SIZE, input);
-	
+
 	if (ferror(input) == -1)
 	{
 		perror("Error: fread() from out end of pipe");
@@ -69,7 +69,7 @@ int main()
 	}
 	make_upper(buf, read_count);
 	fwrite(buf, read_count, 1, stdout);
-	
+
 	if (ferror(input) == -1)
 	{
 		perror("Error: fwrite() in stdout");
