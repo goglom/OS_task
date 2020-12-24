2818942a0af767022efca6ca5e99c0ea8daa18d3
goglom <57570818+goglom@users.noreply.github.com>
2020-12-25 00:44:23 +0900

    Update main.c

--- task_27/main.c
+++ task_27/main.c
@@ -5,10 +5,44 @@
 
 #define BUFFER_SIZE 100
 
+int my_pclose(FILE* pipe)
+{
+	int status = pclose(pipe);
+	if (status == -1)
+	{
+		perror("pclose () error");
+		return -1;
+	}
+	if (WIFEXITED(status))
+	{
+		int exit_status = WEXITSTATUS(status);
+		if (exit_status != 0)
+		{
+			printf("Child exited wtih status: %d\n", exit_status);
+			return -1;
+		}
+	}
+	else if (WIFSIGNALED(status))
+	{
+		printf("killed by signal\n", WTERMSIG(status));
+		return -1;
+	}
+	else if (WIFSTOPPED(status))
+	{
+		printf("stopped by signal: %d\n", WSTOPSIG(status));
+		return -1;
+	}
+	else if (WIFCONTINUED(status))
+	{
+		printf("continued\n");
+	}
+	return 0;
+}
 
 int main(int argc, char* argv[])
 {
 	FILE* input = popen("grep -E '^( )*$' main.c | wc -l", "r");
+	
 	if (input == NULL)
 	{
 		perror("popen() error");
@@ -20,34 +54,11 @@ int main(int argc, char* argv[])
 	if (num_of_lines == NULL && !feof(input))
 	{
 		perror("fgets() error");
-
-    	if (pclose(input) == -1)
-    	{
-			perror("pclose () error");
-		}
-        return EXIT_FAILURE;
-    }
-    int status = pclose(input);
-    if (status == -1)
-    {
-    	perror("pclose () error");
-    	return EXIT_FAILURE;
-    }
-    else if (WIFEXITED(status))
-    {
-	int exit_stat = WEXITSTATUS(status);
-	if (exit_stat ! = 0)
+	}
+    	if (my_pclose(input))
 	{
-    		printf("Child exit with non null exit code: %d\n", WEXITSTATUS(status));
 		return EXIT_FAILURE;
 	}
-    }
-    else if (WIFSIGNALED(status))
-    {
-		printf("Child closed by signal: %d\n", WTERMSIG(status));
-		return EXIT_FAILURE;
-    }
-
 	printf("Number of blank lines: %u\n", (unsigned)strtol(num_of_lines, NULL, 10));
 
 	return EXIT_SUCCESS;
